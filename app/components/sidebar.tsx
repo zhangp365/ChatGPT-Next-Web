13a3212935f3b4d7768765498269b2a3b3e7faab
zhangsongbo <zhangp365@gmail.com>
2023-11-17 18:14:12 +0800

    test title change

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Build your own AI assistant, just do it.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
